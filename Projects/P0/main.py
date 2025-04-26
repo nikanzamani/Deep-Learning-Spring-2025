5e89622d68e9cc6379f899cb7cf269de44814bc6
Nikan Zamani <zamaninikan@gmail.com>
2025-04-26 08:45:33 +0330

    added ID

--- Projects/P0/main.py
+++ Projects/P0/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
+ID="example name:1400xxxxxxxx"
+# the ID field you upload should be in the "<name>:<student number>" format.
+# and you should put your name and student number in this field
+
 def main(n:int,m:int,x:int,vectors:list[list[float]]):
     
     # write your program only here
